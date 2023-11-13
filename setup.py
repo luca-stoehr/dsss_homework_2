3413b8c2cf548d707573ed9237db62dd6b33a184
Luca Stöhr <luca_stoehr@web.de>
2023-11-13 19:19:44 +0100

    update setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import findpackages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as g:
+    required = g.read().splitlines()
+
+setup(
+    name="dsss_homework_2"
+    version="1.0"
+    description="Homework for University. Can you solve the math quiz?"
+    url="https://github.com/luca-stoehr/dsss_homework_2"
+    author="Luca Stöhr"
+    author_email="luca_stoehr@web.de"
+    install_requires=required,
+    python_requires=">=3.9"
+)

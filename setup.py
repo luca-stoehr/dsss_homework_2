51e39c4e41952a1c67074b7bd146236546f2d377
Luca Stöhr <luca_stoehr@web.de>
2023-11-13 19:22:51 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open("requirements.txt") as g:
     required = g.read().splitlines()
 
 setup(
-    name="dsss_homework_2"
-    version="1.0"
-    description="Homework for University. Can you solve the math quiz?"
-    url="https://github.com/luca-stoehr/dsss_homework_2"
-    author="Luca Stöhr"
-    author_email="luca_stoehr@web.de"
+    name="dsss_homework_2",
+    version="1.0",
+    description="Homework for University. Can you solve the math quiz?",
+    url="https://github.com/luca-stoehr/dsss_homework_2",
+    author="Luca Stöhr",
+    author_email="luca_stoehr@web.de",
     install_requires=required,
-    python_requires=">=3.9"
+    python_requires=">=3.9",
 )
